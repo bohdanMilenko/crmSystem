5e54b6be5ab8223de6a7b01be37004f5b61e347d
Bohdan Milenko <bogdan57milenko@gmail.com>
2019-11-08 22:54:47 -0500

    Added new features for RRSP product

--- src/com/bank/RRSP.java
+++ src/com/bank/RRSP.java
@@ -8,6 +8,7 @@ public class RRSP extends FinancialProduct implements Promotion{
     private FinancialClientsInfo financialClientsInfo;
     private double roomForContribution;
     private Map<LocalDate,Double > maximumContributionRoomYearly;
+    private double balance;
 
     public RRSP(FinancialClientsInfo financialClientsInfo) {
         this.financialClientsInfo = financialClientsInfo;
@@ -20,12 +21,27 @@ public class RRSP extends FinancialProduct implements Promotion{
 
     @Override
     void depositMoneyToAccount(double amount) {
+       if(roomForContribution>amount) {
+           balance += amount;
+       }else {
+           System.out.println("Your available room for contribution: $" + roomForContribution);
+       }
 
     }
 
     @Override
     void withdrawMoneyFromAccount(double amount) {
+        double netWithdrawalAmount;
+        if(amount<balance){
+            balance-=amount;
+            netWithdrawalAmount = redeemTaxesFromGrossAmount(amount);
+        }else {
+            System.out.println("You can withdraw only: $" + balance);
+        }
+    }
 
+    private double redeemTaxesFromGrossAmount(double amount){
+        return amount*(1-0.18);
     }
 
     @Override
