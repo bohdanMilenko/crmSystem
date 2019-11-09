bf9676ee472ac871a4beb47ca5c55bed0a1988a0
Bohdan Milenko <bogdan57milenko@gmail.com>
2019-11-08 22:45:39 -0500

    Updated RRSP object with new functionality

--- src/com/bank/RRSP.java
+++ src/com/bank/RRSP.java
@@ -1,8 +1,22 @@
 package com.bank;
 
+import java.time.LocalDate;
+import java.util.Map;
+
 public class RRSP extends FinancialProduct implements Promotion{
 
     private FinancialClientsInfo financialClientsInfo;
+    private double roomForContribution;
+    private Map<LocalDate,Double > maximumContributionRoomYearly;
+
+    public RRSP(FinancialClientsInfo financialClientsInfo) {
+        this.financialClientsInfo = financialClientsInfo;
+        this.roomForContribution = calculateRoom();
+    }
+
+    private double calculateRoom(){
+
+    }
 
     @Override
     void depositMoneyToAccount(double amount) {
