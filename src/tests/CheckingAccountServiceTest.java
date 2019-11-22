9fa36875c338ef9df9c50d6b9152ba45f1d8ce2f
Bohdan Milenko <bogdan57milenko@gmail.com>
2019-11-21 22:55:15 -0500

    Refactored tests

--- src/tests/CheckingAccountServiceTest.java
+++ src/tests/CheckingAccountServiceTest.java
@@ -9,6 +9,7 @@ import com.bank.Service.ClientAccountService;
 import java.time.LocalDate;
 
 import com.bank.Service.FinancialProductService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,65 +18,65 @@ import static org.junit.jupiter.api.Assertions.*;
 class CheckingAccountServiceTest {
 
     private CheckingAccount checkingAccount;
-    private CheckingAccountService checkingAccountService;
+    private ClientAccount clientAccount;
+
+    private ClientAccountService clientAccountService = new ClientAccountService();
+    private CheckingAccountService checkingAccountService = new CheckingAccountService();
 
 
-//    @BeforeEach
-//    void setUp() {
-//        Customer customer = new Customer("Bob", "Marley", LocalDate.of(1965, 10, 1), false, false);
-//        ClientAccount clientAccount = new ClientAccount(customer);
-//        ClientAccountService clientAccountService = new ClientAccountService(clientAccount);
-//        checkingAccountService = clientAccountService.openCheckingAccount(15000);
-//        checkingAccount = (CheckingAccount) clientAccount.getTypeToFinancialProductMap().get(FinancialProductService.FinancialProductType.CHECKING_ACCOUNT);
-//
-//    }
-//
-//    @Test
-//    void testDepositMoneyToAccount() {
-//
-//        checkingAccountService.depositMoneyToAccount(10000.00);
-//        assertEquals(25000 - FinancialProductService.CHECKING_ACCOUNT_YEARLY_FEE, checkingAccount.getBalance(), 0.000001);
-//    }
-//
-//    @Test
-//    void testWithdrawMoneyFromAccountWithinLimits() {
-//        checkingAccountService.withdrawMoneyFromAccount(10000);
-//        assertEquals(5000 - FinancialProductService.CHECKING_ACCOUNT_YEARLY_FEE, checkingAccount.getBalance());
-//
-//    }
-//
-//    @Test
-//    void testWithdrawMoneyFromAccountOverLimit() throws Exception {
-//
-//        checkingAccountService.withdrawMoneyFromAccount(20000);
-//        assertEquals(15000 - FinancialProductService.CHECKING_ACCOUNT_YEARLY_FEE, checkingAccount.getBalance());
-//
-//    }
-//
-//    @Test
-//    void testPrintTransactionHistory() {
-//    }
-//
-//    @Test
-//    void testReviewBalance() {
-//    }
-//
-//    @Test
-//    void viewEligibilityTerms() {
-//    }
-//
-//    @Test
-//    void checkIfEligibleForPromotionNOoExpenses() {
-//        assertFalse(checkingAccount.isEligibleForPromotion());
-//    }
-//
-//    @Test
-//    void checkIfEligibleForPromotionWithExpenses() {
-//        checkingAccountService.withdrawMoneyFromAccount(10000);
-//        assertTrue( checkingAccountService.checkIfEligibleForPromotion());
-//    }
-//
-//    @Test
-//    void applyPromotion() {
-//    }
+    @BeforeEach
+    void setUp() {
+        Customer customer = new Customer("Bob", "Marley", LocalDate.of(1965, 10, 1), false, false);
+        clientAccount = new ClientAccount(customer);
+        checkingAccount = (CheckingAccount) clientAccount.getTypeToFinancialProductMap().get(FinancialProductService.FinancialProductType.CHECKING_ACCOUNT);
+
+    }
+
+    @Test
+    void testDepositMoneyToAccount() {
+        checkingAccountService.depositMoneyToAccount(clientAccount, 25000);
+        assertEquals(25000 - FinancialProductService.CHECKING_ACCOUNT_YEARLY_FEE, checkingAccount.getBalance(), 0.000001);
+    }
+
+    @Test
+    void testWithdrawMoneyFromAccountWithinLimits() throws Exception {
+        checkingAccountService.withdrawMoneyFromAccount(clientAccount, 10000);
+        assertEquals(5000 - FinancialProductService.CHECKING_ACCOUNT_YEARLY_FEE, checkingAccount.getBalance());
+
+    }
+
+    @Test
+    void testWithdrawMoneyFromAccountOverLimit() throws Exception {
+
+        checkingAccountService.withdrawMoneyFromAccount(clientAccount, 20000);
+        assertEquals(15000 - FinancialProductService.CHECKING_ACCOUNT_YEARLY_FEE, checkingAccount.getBalance());
+
+    }
+
+    @Test
+    void testPrintTransactionHistory() {
+    }
+
+    @Test
+    void testReviewBalance() {
+    }
+
+    @Test
+    void viewEligibilityTerms() {
+    }
+
+    @Test
+    void checkIfEligibleForPromotionNOoExpenses() {
+        assertFalse(checkingAccount.isEligibleForPromotion());
+    }
+
+    @Test
+    void checkIfEligibleForPromotionWithExpenses() throws Exception{
+        checkingAccountService.withdrawMoneyFromAccount(clientAccount,1000);
+        assertTrue(checkingAccountService.checkIfEligibleForPromotion(clientAccount));
+    }
+
+    @Test
+    void applyPromotion() {
+    }
 }
